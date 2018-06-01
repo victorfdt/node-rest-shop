9b6a9e9854d00690b0a4df3bb6a0d09643a3516a
Victor F <victorfdt@yahoo.com.br>
2018-06-01 10:34:43 -0300

    Handling errors

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+//Logging feature
+//Morgan will use the next to move to the next middleware
+app.use(morgan('dev'));
+
 //the method use creates a middleware. So, every incoming
 //requests will have to pass throw it.
-
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//Handling Not found route error
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+
+    //It will forward the request with the error
+    next(error); 
+});
+
+//Handling any other kind of error
+app.use((error, req, res, next) => {
+    error.status = error.status || 500;
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
