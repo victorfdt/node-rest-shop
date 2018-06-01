c4645a82d7378ea04eb2a65f34f7b2a60f67b408
Victor F <victorfdt@yahoo.com.br>
2018-06-01 09:30:43 -0300

    Improving routes

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
 //the method use creates a middleware. So, every incoming
 //requests will have to pass throw it.
-app.use((req, res, next) => {
 
-    //It respondes a 200 status in the json format
-    res.status(200).json({
-        message: "It works!"
-    });
-});
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
