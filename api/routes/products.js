8f853fee0c01c652dcc4f324336137fc433696c8
Victor F <victorfdt@yahoo.com.br>
2018-06-03 12:08:45 -0300

    Improving request messages | Mongoose validation

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,9 +8,24 @@ const Product = require('../models/product');
 //Get all recorded products
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
+        .exec()
         .then(docs => {
-            console.log(docs);
-            res.status(200).json(docs);
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        name: doc.name,
+                        price: doc.price,
+                        request: {
+                            type: "GET",
+                            url: "http://localhost:3000/products/" + doc._id
+                        }
+                    }
+                })
+            };
+            res.status(200).json(response);
         })
         .catch(err => {
             console.log(err);
@@ -31,10 +46,18 @@ router.post('/', (req, res, next) => {
     //Store in the database
     product.save()
         .then(result => {
-            console.log(result);
             res.status(201).json({
-                message: 'Handling POST requests to /products',
-                createProduct: result
+                message: 'Product created successfuly',
+                createProduct: {
+                    _id: result._id,
+                    name: result.name,
+                    price: result.price,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products' + result._id
+                    }
+                }
+
             });
         })
         .catch(err => {
@@ -50,12 +73,18 @@ router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
 
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
             console.log(doc);
-
             if (doc) {
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products' + doc._id
+                    }
+                });
             } else {
                 res.status(404).json({
                     message: 'No valid entry found for provided id'
@@ -75,35 +104,47 @@ router.patch("/:productId", (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
     for (const ops of req.body) {
-      updateOps[ops.propName] = ops.value;
+        updateOps[ops.propName] = ops.value;
     }
     Product.update({ _id: id }, { $set: updateOps })
-      .exec()
-      .then(result => {
-        console.log(result);
-        res.status(200).json(result);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Product updated!',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products' + result._id
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
         });
-      });
-  });
+});
 
 router.delete("/:productId", (req, res, next) => {
     const id = req.params.productId;
     Product.remove({ _id: id })
-      .exec()
-      .then(result => {
-        res.status(200).json(result);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Product deleted!',
+                request: {
+                    type: 'GET',
+                    description: 'Get all products',
+                    url: 'http://localhost:3000/products'
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
         });
-      });
-  });
+});
 
 module.exports = router;
